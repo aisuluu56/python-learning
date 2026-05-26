626bd9ab823de70e97e0359cf1744a89d09ccbc2
Elraden <morozovsa94@yandex.ru>
2026-05-26 12:56:43 +0300

    Add Python syntax practice materials

---
+++ 07_dictionaries/tasks.py
@@ -0,0 +1,68 @@
+"""
+Тема 7. Словари
+"""
+
+
+# Задание 1
+# Создайте словарь student с ключами:
+# name, age, group.
+# Выведите каждое значение отдельно.
+
+
+# TODO: решение
+
+
+# Задание 2
+# Создайте словарь product с ключами title, price, count.
+# Посчитайте общую стоимость товара: price * count.
+
+
+# TODO: решение
+
+
+# Задание 3
+# Дан словарь user = {"name": "Ivan", "email": "ivan@example.com"}.
+# Добавьте ключ age со значением 20.
+# Измените email на другой.
+# Выведите итоговый словарь.
+
+user = {"name": "Ivan", "email": "ivan@example.com"}
+
+# TODO: решение
+
+
+# Задание 4
+# Дан словарь scores = {"Anna": 5, "Ivan": 4, "Maria": 5, "Petr": 3}.
+# Выведите имена студентов, у которых оценка 5.
+
+scores = {"Anna": 5, "Ivan": 4, "Maria": 5, "Petr": 3}
+
+# TODO: решение
+
+
+# Задание 5
+# Спросите у пользователя название страны.
+# Есть словарь capitals:
+# Russia -> Moscow
+# France -> Paris
+# Germany -> Berlin
+# Если страна есть в словаре, выведите столицу.
+# Если нет, выведите "Страна не найдена".
+
+capitals = {
+    "Russia": "Moscow",
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+# TODO: решение
+
+
+# Задание 6
+# Дан текст text = "apple banana apple orange banana apple".
+# Посчитайте, сколько раз встречается каждое слово.
+# Результат должен быть словарем.
+
+text = "apple banana apple orange banana apple"
+
+# TODO: решение
